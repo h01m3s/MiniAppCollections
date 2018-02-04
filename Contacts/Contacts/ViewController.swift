ef2f5a50c9ccea44cc0c20019bbbeac722c15ea5
h01m3s <lin426797@gmail.com>
2018-02-03 23:23:12 -0500

    tableView sections

--- Contacts/Contacts/ViewController.swift
+++ Contacts/Contacts/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let cellId = "cellId"
+
+    let twoDimensionalArray = [
+        ["Amy", "Bill", "Zack", "Steve", "Jack"],
+        ["Carl", "Chris", "Christina", "Cameron"],
+        ["David", "Dan"],
+        ["Paker", "Patty"]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Contacts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Header"
+        label.backgroundColor = UIColor.lightGray
+        return label
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return twoDimensionalArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return twoDimensionalArray[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        let name = twoDimensionalArray[indexPath.section][indexPath.row]
+        
+        cell.textLabel?.text = name
+        
+        cell.textLabel?.text = "\(name) Section:\(indexPath.section) Row:\(indexPath.row)"
+        
+        return cell
+    }
 
 }
 
