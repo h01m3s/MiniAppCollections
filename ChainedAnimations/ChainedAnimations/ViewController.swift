95ab8f946522ae7669329a60c725f35595376132
h01m3s <lin426797@gmail.com>
2018-03-30 13:55:33 -0400

    Simple chain animations

---
+++ ChainedAnimations/ChainedAnimations/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  ChainedAnimations
+//
+//  Created by Weijie Lin on 3/30/18.
+//  Copyright © 2018 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let titleLabel = UILabel()
+    let bodyLabel = UILabel()
+
+    fileprivate func setupLabels() {
+        titleLabel.numberOfLines = 0
+        titleLabel.text = "Welcome To Company AAA"
+        titleLabel.font = UIFont(name: "Futura", size: 34)
+        bodyLabel.numberOfLines = 0
+        bodyLabel.text = "Hello there! Thanks so much for downloading our brand new app and giving us a try. Make sure to leave us a good review in the App Store."
+    }
+    
+    fileprivate func setupStackView() {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, bodyLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        view.addSubview(stackView)
+        // Enables autolayout
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -100).isActive = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupLabels()
+        setupStackView()
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapAnimations)))
+    }
+    
+    @objc fileprivate func handleTapAnimations() {
+        
+        // Animate titleLabel
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            
+            self.titleLabel.transform = CGAffineTransform(translationX: -30, y: 0)
+            
+        }) { (_) in
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                
+                self.titleLabel.alpha = 0
+                self.titleLabel.transform = self.titleLabel.transform.translatedBy(x: 0, y: -200)
+                
+            })
+        }
+        
+        // animate bodyLabel
+        UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            
+            self.bodyLabel.transform = CGAffineTransform(translationX: -30, y: 0)
+            
+        }) { (_) in
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                
+                self.bodyLabel.alpha = 0
+                self.bodyLabel.transform = self.bodyLabel.transform.translatedBy(x: 0, y: -200)
+                
+            })
+        }
+    }
+
+}
+
