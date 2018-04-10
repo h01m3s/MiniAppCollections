145d8deac7d889c6d4c92f69eefc2855666e5271
h01m3s <lin426797@gmail.com>
2018-04-09 22:13:16 -0400

    ObjcGettingStarted init

---
+++ ObjcGettingStarted/ObjcGettingStarted/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjcGettingStarted
+//
+//  Created by Weijie Lin on 4/9/18.
+//  Copyright © 2018 Weijie Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjcGettingStarted/ObjcGettingStarted/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjcGettingStarted
+//
+//  Created by Weijie Lin on 4/9/18.
+//  Copyright © 2018 Weijie Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
