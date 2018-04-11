33403cad07c4825a3d8d9422ee41b1ce99b59d5f
h01m3s <lin426797@gmail.com>
2018-04-11 13:49:16 -0400

    fetch and parse json data

--- ObjcGettingStarted/ObjcGettingStarted/ViewController.m
+++ ObjcGettingStarted/ObjcGettingStarted/ViewController.m
@@ -23,11 +23,51 @@ NSString *cellId = @"cellId";
     [super viewDidLoad];
     
     [self setupCourses];
+    [self fetchCoursesUsingJson];
     
     self.navigationItem.title = @"Courses";
     self.navigationController.navigationBar.prefersLargeTitles = YES;
     
     [self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier:cellId];
+}
+
+- (void)fetchCoursesUsingJson {
+    NSLog(@"Fetching Courses");
+//    http://api.letsbuildthatapp.com/jsondecodable/courses
+    
+    NSString *urlString = @"https://api.letsbuildthatapp.com/jsondecodable/courses";
+    NSURL *url = [NSURL URLWithString:urlString];
+    [[NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSLog(@"finished fetching courses...");
+        NSString *dummyString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"Dummy String: %@", dummyString);
+        
+        NSError *err;
+        NSArray *courseJson = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&err];
+        
+        if (err) {
+            NSLog(@"Failed to serialize into JSON: %@", err);
+            return;
+        }
+        
+        NSMutableArray<Course *> *courses = NSMutableArray.new;
+        for (NSDictionary *courseDict in courseJson) {
+            NSString *name = courseDict[@"name"];
+            NSNumber *numberOflessons = courseDict[@"number_of_lessons"];
+            Course *course = Course.new;
+            course.name = name;
+            course.numberOfLessons = numberOflessons;
+            [courses addObject:course];
+        }
+        
+//        NSLog(@"%@", courses);
+        self.courses = courses;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+        
+    }] resume];
 }
 
 - (void)setupCourses {
@@ -45,10 +85,12 @@ NSString *cellId = @"cellId";
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
     
     Course *course = self.courses[indexPath.row];
     cell.textLabel.text = course.name;
+    cell.detailTextLabel.text = course.numberOfLessons.stringValue;
     return cell;
 }
 
