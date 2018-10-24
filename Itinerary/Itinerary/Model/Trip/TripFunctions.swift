3b0434dce57eafa3e5c03cc306637f79ee4c70db
h01m3s <lin426797@gmail.com>
2018-10-23 23:03:50 -0400

    TripModel and TripFunction created.

---
+++ Itinerary/Itinerary/Model/Trip/TripFunctions.swift
@@ -0,0 +1,9 @@
+//
+//  TripFunctions.swift
+//  Itinerary
+//
+//  Created by Weijie Lin on 10/23/18.
+//  Copyright © 2018 Weijie Lin. All rights reserved.
+//
+
+import Foundation
---
+++ Itinerary/Itinerary/Model/Trip/TripModel.swift
@@ -0,0 +1,9 @@
+//
+//  TripModel.swift
+//  Itinerary
+//
+//  Created by Weijie Lin on 10/23/18.
+//  Copyright © 2018 Weijie Lin. All rights reserved.
+//
+
+import Foundation
