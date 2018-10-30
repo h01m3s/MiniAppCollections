ea85d17d3465d8a3122b7b404163c21be63a84fb
h01m3s <lin426797@gmail.com>
2018-10-29 20:48:03 -0400

    added readTrips.

---
+++ Itinerary/Itinerary/Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  Itinerary
+//
+//  Created by Weijie Lin on 10/29/18.
+//  Copyright © 2018 Weijie Lin. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    static var tripModels = [TripModel]()
+    
+}
