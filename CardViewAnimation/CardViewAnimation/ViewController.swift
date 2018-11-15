5ad3b8dd5cbdc6877f6206da78aa17afff6a62c5
h01m3s <lin426797@gmail.com>
2018-11-14 23:23:18 -0500

    CardViewAnimation project init.

---
+++ CardViewAnimation/CardViewAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardViewAnimation
+//
+//  Created by Weijie Lin on 11/14/18.
+//  Copyright © 2018 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
