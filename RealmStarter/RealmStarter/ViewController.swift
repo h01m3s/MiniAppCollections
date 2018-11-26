929c3e19a50182c7e7a811c6b0fa5d4abfefca04
h01m3s <lin426797@gmail.com>
2018-11-25 22:19:16 -0500

    RealmStarter init.

---
+++ RealmStarter/RealmStarter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmStarter
+//
+//  Created by Weijie Lin on 11/25/18.
+//  Copyright © 2018 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
