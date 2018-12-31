1d07f5af6b2da01a815cfff76268b25d6617bc9a
h01m3s <lin426797@gmail.com>
2018-12-31 15:30:53 -0500

    Retain Cycle implemented.

--- RetainCycleInstruments/RetainCycleInstruments/ViewController.swift
+++ RetainCycleInstruments/RetainCycleInstruments/ViewController.swift
@@ -12,9 +12,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Show Red", style: .plain, target: self, action: #selector(handleShowRedController))
+    }
+    
+    @objc func handleShowRedController() {
+        navigationController?.pushViewController(RedController(), animated: true)
     }
 
 
+}
+
+class Service {
+    
+    var redController: RedController?
+    
+    // use weak to resolve retain cycle
+    // all varibles are "strong" in default
+//    weak var redController: RedController?
+    
+}
+
+class RedController: UITableViewController {
+    
+    deinit {
+        print("OS Reclaiming Memory For RedController")
+    }
+    
+    let service = Service()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = .red
+        
+        // Retain Cycle Created
+        service.redController = self
+    }
+    
 }
 
