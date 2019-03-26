1da15bb359c757c16eeb4c5fa505523435104b05
h01m3s <lin426797@gmail.com>
2019-03-25 23:49:04 -0400

    NumPad init.

---
+++ NumPad/NumPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NumPad
+//
+//  Created by Weijie Lin on 3/25/19.
+//  Copyright © 2019 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
