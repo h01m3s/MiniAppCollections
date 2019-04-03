ead8a6f11c1f1bc6b4bc315695fc9363e4a8d264
h01m3s <lin426797@gmail.com>
2019-04-02 23:55:23 -0400

    ResultType init

---
+++ ResultType/ResultType/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResultType
+//
+//  Created by Weijie Lin on 4/2/19.
+//  Copyright © 2019 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
