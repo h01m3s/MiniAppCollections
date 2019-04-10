31573733e3f95809cb178497b6a0ea227e7bf164
h01m3s <lin426797@gmail.com>
2019-04-09 23:27:37 -0400

    Playgound updated.

--- Playgrounds/compactMap_and_flatMap.playground/Contents.swift
+++ Playgrounds/compactMap_and_flatMap.playground/Contents.swift
@@ -6,3 +6,17 @@ struct Store {
     var electronicHardware: [String]?
     
 }
+
+let target = Store(name: "Target", electronicHardware: [
+    "iPhone", "iPad", "Flatscreen TVs"
+    ])
+
+let bestBuy = Store(name: "Best Buy", electronicHardware: ["Laptops", "Big Fridges"])
+
+let bedBathAndBeyound = Store(name: "Bed Bath & Beyound", electronicHardware: nil)
+
+//let items = target.electronicHardware! + bestBuy.electronicHardware! + bedBathAndBeyound.electronicHardware!
+
+let items2 = [target, bestBuy, bedBathAndBeyound].compactMap { $0.electronicHardware }.flatMap{$0}
+
+print(items2)
