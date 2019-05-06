2b5e305d4eb2377350fb73585b11fab2939ace35
h01m3s <lin426797@gmail.com>
2019-05-05 22:49:35 -0400

    Anchored Gradient Layers

--- StretchyHeader/StretchyHeader/HeaderView.swift
+++ StretchyHeader/StretchyHeader/HeaderView.swift
@@ -25,6 +25,43 @@ class HeaderView: UICollectionReusableView {
         
         // blur
         setupVisualEffectBlur()
+        
+        setupGradientLayer()
+    }
+    
+    fileprivate func setupGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.5, 1]
+        
+//        layer.addSublayer(gradientLayer)
+        let gradientContainerView = UIView()
+        addSubview(gradientContainerView)
+        gradientContainerView.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+        
+        gradientContainerView.layer.addSublayer(gradientLayer)
+        // static frame
+        gradientLayer.frame = self.bounds
+        
+       gradientLayer.frame.origin.y -= bounds.height
+        
+        let heavyLabel = UILabel()
+        heavyLabel.text = "Surf the web for courses"
+        heavyLabel.font = .systemFont(ofSize: 24, weight: .heavy)
+        heavyLabel.textColor = .white
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "description label description label description label description label description label"
+        descriptionLabel.font = .systemFont(ofSize: 14, weight: .regular)
+        descriptionLabel.textColor = .white
+        descriptionLabel.numberOfLines = 0
+        
+        let stackView = UIStackView(arrangedSubviews: [heavyLabel, descriptionLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        addSubview(stackView)
+        stackView.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 16, right: 16))
     }
     
     var animator: UIViewPropertyAnimator!
@@ -39,6 +76,8 @@ class HeaderView: UICollectionReusableView {
             self?.addSubview(visualEffectView)
             visualEffectView.fillSuperview()
         })
+        
+        animator.fractionComplete = 0
         
     }
     
