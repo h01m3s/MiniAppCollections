e3d2e014a25b879108039b595dc02f7266d3bacc
h01m3s <lin426797@gmail.com>
2019-05-23 23:28:55 -0400

    ScrollView_Keyboard init.

---
+++ ScrollView_Keyboard/ScrollView_Keyboard/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ScrollView_Keyboard
+//
+//  Created by Weijie Lin on 5/22/19.
+//  Copyright © 2019 Weijie Lin. All rights reserved.
+//
+
+import LBTATools
+
+class ViewController: LBTAFormController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0.9490136504, green: 0.941204071, blue: 0.9488920569, alpha: 1)
+    }
+
+
+}
+
