ad329ae6a9ea9bb9f55024f11624202115f27424
h01m3s <lin426797@gmail.com>
2019-05-25 23:45:49 -0400

    Login form complete.

--- ScrollView_Keyboard/ScrollView_Keyboard/AppDelegate.swift
+++ ScrollView_Keyboard/ScrollView_Keyboard/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         window = UIWindow()
-        window?.rootViewController = ViewController(alignment: .top)
+        window?.rootViewController = ViewController(alignment: .center)
         
         return true
     }
--- ScrollView_Keyboard/ScrollView_Keyboard/ViewController.swift
+++ ScrollView_Keyboard/ScrollView_Keyboard/ViewController.swift
@@ -9,12 +9,62 @@
 import LBTATools
 
 class ViewController: LBTAFormController {
+    
+    let signUpButton = UIButton(title: "Sign Up", titleColor: .white, font: .boldSystemFont(ofSize: 16), backgroundColor: #colorLiteral(red: 0.2901960784, green: 0.5058823529, blue: 0.9490196078, alpha: 1), target: self, action: #selector(handleCancel))
+    let cancelButton = UIButton(title: "Cancel", titleColor: .white, font: .boldSystemFont(ofSize: 16), backgroundColor: .red, target: self, action: #selector(handleCancel))
+    
+    @objc fileprivate func handleCancel() {
+        dismiss(animated: true)
+    }
+    
+    let fbButton = UIButton(image: #imageLiteral(resourceName: "fb_circle.png").withRenderingMode(.alwaysOriginal))
+    let twitterButton = UIButton(image: #imageLiteral(resourceName: "twitter_circle.png").withRenderingMode(.alwaysOriginal))
+    let gitHubButton = UIButton(image: #imageLiteral(resourceName: "github_circle.png").withRenderingMode(.alwaysOriginal))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = #colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.9490196078, alpha: 1)
+        
+        scrollView.alwaysBounceVertical = true
+        
+        formContainerStackView.axis = .vertical
+        formContainerStackView.spacing = 12
+        formContainerStackView.layoutMargins = .init(top: 0, left: 24, bottom: 16, right: 24)
+        
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "logo"), contentMode: .scaleAspectFit)
+        imageView.constrainHeight(64)
+        
+        formContainerStackView.addArrangedSubview(imageView)
+        
+        (0...1).forEach{ (_) in
+            let textField = IndentedTextField(placeholder: "Enter text", padding: 12, cornerRadius: 5, backgroundColor: .white)
+            textField.constrainHeight(60)
+            formContainerStackView.addArrangedSubview(textField)
+        }
+        
+        formContainerStackView.addArrangedSubview(signUpButton)
+        formContainerStackView.addArrangedSubview(cancelButton)
+        signUpButton.constrainHeight(60)
+        cancelButton.constrainHeight(60)
+        signUpButton.roundViewEdge(radius: 5)
+        cancelButton.roundViewEdge(radius: 5)
+        
+        let buttonsStackView = UIStackView(arrangedSubviews: [fbButton, twitterButton, gitHubButton])
+        [fbButton, twitterButton, gitHubButton].forEach {$0.imageView?.contentMode = .scaleAspectFit }
+        buttonsStackView.constrainHeight(50)
+        buttonsStackView.distribution = .fillEqually
+        formContainerStackView.addArrangedSubview(buttonsStackView)
     }
 
+}
+
+extension UIView {
+    
+    func roundViewEdge(radius: CGFloat) {
+        self.layer.cornerRadius = radius
+        self.layer.masksToBounds = true
+    }
+    
 }
 
