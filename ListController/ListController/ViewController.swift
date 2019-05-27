9ec2bf14093511c5454a7db3b64fb46941a9fd38
h01m3s <lin426797@gmail.com>
2019-05-26 23:08:59 -0400

    ListController init.

---
+++ ListController/ListController/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ListController
+//
+//  Created by Weijie Lin on 5/26/19.
+//  Copyright © 2019 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+
+}
+
