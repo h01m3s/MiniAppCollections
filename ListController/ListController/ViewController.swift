98b1556bc4d811059c2f898d7f7b03a236c0f688
h01m3s <lin426797@gmail.com>
2019-05-27 23:21:10 -0400

    Basic setup.

--- ListController/ListController/ViewController.swift
+++ ListController/ListController/ViewController.swift
@@ -6,16 +6,49 @@
 //  Copyright © 2019 Weijie Lin. All rights reserved.
 //
 
-import UIKit
+import LBTATools
 
-class ViewController: UIViewController {
+class MessageCell: LBTAListCell<UIColor> {
+    
+    let imageView = UIImageView(backgroundColor: .gray)
+    let nameLabel = UILabel(text: "User name", font: .boldSystemFont(ofSize: 16))
+    let messageLabel = UILabel(text: "Hey, what's up.", font: .systemFont(ofSize: 14), textColor: .gray, numberOfLines: 2)
+    
+    override var item: UIColor! {
+        didSet {
+            backgroundColor = item
+        }
+    }
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        imageView.layer.cornerRadius = 64 / 2
+        
+        hstack(imageView.withWidth(64).withHeight(64),
+               stack(nameLabel, messageLabel, spacing: 4),
+               spacing: 12,
+               alignment: .center
+               ).withMargins(.allSides(16))
+    }
+    
+}
+
+class MessagesController: LBTAListController<MessageCell, UIColor>, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .white
+        
+        navigationItem.title = "Messages"
+        
+        items = [
+            .red, .blue, .yellow, .green, .purple
+        ]
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 100)
+    }
 
 }
 
