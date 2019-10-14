dcb668db558e4b58f3744b1c2bffaaf4b5646a9c
h01m3s <lin426797@gmail.com>
2019-10-14 00:16:40 -0400

    Modals And Binding init.

---
+++ ModalsAndBinding/ModalsAndBinding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ModalsAndBinding
+//
+//  Created by h01m3s on 10/14/19.
+//  Copyright © 2019 h01m3s. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
